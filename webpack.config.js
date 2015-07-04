9db53dd28f13c6cc88944ecb0bb5b052b3e07752
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2015-07-04 21:35:54 +0200

    with live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   entry: {
     main: [
+     'webpack-dev-server/client?http://localhost:8080',
+     'webpack/hot/only-dev-server',
       './components/main.js'
     ]
   },
   output: {
-    filename: './public/main.js'
+    filename: './public/main.js',
+    publicPath: '/'
   }
 }
