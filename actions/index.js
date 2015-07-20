083a0c3311be5ebb042af06a75d65141cef1bf00
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2015-07-20 23:27:31 +0200

    removed old misguided code to init channels

--- actions/index.js
+++ actions/index.js
@@ -23,51 +23,10 @@ class Actions {
       AuthService.startAuth()
         .then((result)=> {
           dispatch(result);
-          this.initChannels();
+          // this.initChannels();
         });
     }
   }
-
-  // initChannels(){
-  //   return (dispatch)=> {
-  //     ChannelService.initChannels()
-  //       .then((result)=> {
-  //         dispatch(result);
-  //       });
-  //   }
-  // }
 }
 
 export default alt.createActions(Actions);
-
-
-// class Actions {
-//   sendMessage(message){
-//     AppDispatcher.dispatch({
-//       actionType: AppConstants.CHAT_SEND_MESSAGE,
-//       message: message
-//     });
-//   }
-//
-//   readMessage(message){
-//     AppDispatcher.dispatch({
-//       actionType: AppConstants.CHAT_READ_MESSAGE,
-//       message: message
-//     });
-//   }
-//
-//   startAuth(){
-//     AppDispatcher.dispatch({
-//       actionType: AppConstants.AUTH_START
-//     });
-//   }
-//
-//   openChannel(channel){
-//     AppDispatcher.dispatch({
-//       actionType: AppConstants.CHANNEL_OPEN,
-//       channel: channel
-//     });
-//   }
-// }
-
-// module.exports = new Actions();
